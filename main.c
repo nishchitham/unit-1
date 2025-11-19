c1f53c8bf6f89edd47d005726a9dcfcf533bfb9f
nishchitha m <nishchitham11@gmail.com>
2025-11-19 07:36:47 +0530

    program for output statement

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
-
 #include <stdio.h>
-
-int main() {
-    char input[100];
-
-    printf("Enter some text: ");
-    fgets(input, sizeof(input), stdin);
-
-    printf("You entered: %s", input);
-
+int main()
+{
+   //output a simple string
+    printf("Hello world!\n");
+    //output with variable
+    int age =25;
+    float height =5.2;
+    char name []="nishchitha";
+    printf("name:%d\n",name);
+    printf ("age:%d years\n",age);
+    printf("height :.1ffeet\n",height);
     return 0;
 }
