311b772f527aadc3488f63f8aa6e1c9a4e0262ae
nishchitha m <nishchitham11@gmail.com>
2025-11-19 07:44:41 +0530

    program for receiving input from users at runtime printing it

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
+
 #include <stdio.h>
-int main()
-{
-   //output a simple string
-    printf("Hello world!\n");
-    //output with variable
-    int age =25;
-    float height =5.2;
-    char name []="nishchitha";
-    printf("name:%d\n",name);
-    printf ("age:%d years\n",age);
-    printf("height :.1ffeet\n",height);
+
+int main() {
+    char input[100];
+
+    printf("Enter some text: ");
+    fgets(input, sizeof(input), stdin);
+
+    printf("You entered: %s", input);
+
     return 0;
 }
